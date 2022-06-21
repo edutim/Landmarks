a62cd196a4893534e4c403244dfbfaa0ec1cb0af
Tim Hart <timothy.hart@gmail.com>
2022-06-21 09:32:03 -0400

    End of Day One

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,27 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Timothy Hart on 6/20/22.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle()
+                    .stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+            
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
