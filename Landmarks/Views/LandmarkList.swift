80623bb8a0e54806fe7f22f9074f32fa2e91c6ce
Tim Hart <timothy.hart@gmail.com>
2022-06-23 13:59:06 -0400

    Final

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -9,11 +9,15 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        List {
-            LandmarkRow(landmark: landmarks[0])
-            LandmarkRow(landmark: landmarks[1])
-            LandmarkRow(landmark: landmarks[2])
-            
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
         }
     }
 }
--- Landmarks/Views/MapView.swift
+++ Landmarks/Views/MapView.swift
@@ -9,16 +9,19 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
-    
-    @State var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 34.011286, longitude: -116.166868), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    var coordinate: CLLocationCoordinate2D
+    @State var region = MKCoordinateRegion()
     
     var body: some View {
         Map(coordinateRegion: $region)
+            .onAppear {
+                region = MKCoordinateRegion(center: coordinate, span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+            }
     }
 }
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(coordinate: CLLocationCoordinate2D(latitude: 34.011286, longitude: -116.166868))
     }
 }
